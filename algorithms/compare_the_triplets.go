00d140ce3dee68065e1d8ad4a408971d65700fad
Raphaela <90319461+raphaelavergud@users.noreply.github.com>
2025-10-24 15:53:56 +0200

    readability of compare the triplets

--- algorithms/compare_the_triplets.go
+++ algorithms/compare_the_triplets.go
@@ -4,19 +4,74 @@ func compareTriplets(a []int32, b []int32) []int32 {
 	var aliceScore int32 = 0
 	var bobScore int32 = 0
 
-	var categoryIndex int = 0
+	var categoryIndex = 0
 
 	for categoryIndex < 3 {
 
-		var aliceRating int32 = a[categoryIndex]
-		var bobRating int32 = b[categoryIndex]
+		var aliceRating = a[categoryIndex]
+		var bobRating = b[categoryIndex]
 
 		if aliceRating > bobRating {
 			aliceScore = aliceScore + 1
-		} else if bobRating > aliceRating {
+		} else if aliceRating < bobRating {
 			bobScore = bobScore + 1
 		}
 		categoryIndex = categoryIndex + 1
+	}
+	var finalScores = []int32{aliceScore, bobScore}
+
+	return finalScores
+}
+
+// more readable: refactor
+
+func compareTriplets2(a []int32, b []int32) []int32 {
+
+	aliceScore := int32(0)
+	bobScore := int32(0)
+
+	for categoryIndex := 0; categoryIndex < 3; categoryIndex++ {
+
+		aliceRating := a[categoryIndex]
+		bobRating := b[categoryIndex]
+
+		if aliceRating > bobRating {
+			aliceScore = aliceScore + 1
+			continue
+		}
+		if aliceRating < bobRating {
+			bobScore = bobScore + 1
+			continue
+		}
+
+	}
+	var finalScores []int32 = []int32{aliceScore, bobScore}
+
+	return finalScores
+}
+
+// most readable:
+
+func compareTriplets3(a []int32, b []int32) []int32 {
+
+	aliceScore := int32(0)
+	bobScore := int32(0)
+
+	for categoryIndex := 0; categoryIndex < 3; categoryIndex++ {
+
+		aliceRating := a[categoryIndex]
+		bobRating := b[categoryIndex]
+
+		if aliceRating == bobRating {
+			continue
+		}
+
+		switch result := aliceRating > bobRating; result {
+		case true:
+			aliceScore += 1
+		case false:
+			bobScore += 1
+		}
 	}
 	var finalScores []int32 = []int32{aliceScore, bobScore}
 
